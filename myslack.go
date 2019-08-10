4ddb802be21e245c9dab2f500d0f415afb389b69
YueMing Qin <aca_jingru@outlook.com>
2019-08-10 15:10:10 +0800

    clean commits. add log in func NewDefaultTokenFromEnv(). add defer log.SetPrefix().

--- myslack.go
+++ myslack.go
@@ -6,54 +6,26 @@ import (
 	"os"
 )
 
-//var SLACKTOKEN string
-//var slackToken string
-
-//var CHANNELID string
-//var channelId string
-//var slackClient *slack.Client
-
 type MySlackClient struct {
 	slackClient *slack.Client
 	slackToken  string
 	channelId   string
 }
 
-//func (client *MySlackClient)NewClient() MySlackClient {
-//	return MySlackClient{}
-//}
-
-func init() {
-	//MySlackClient{slackToken:}
-}
 func NewDefaultTokenFromEnv(channelId string) MySlackClient {
 	log.SetPrefix("[NewDefaultTokenFromEnv()]")
+	defer log.SetPrefix("")
 	slackToken := os.Getenv("SLACKTOKEN")
+	log.Println("get key from env:", slackToken)
 	ret := MySlackClient{slackToken: slackToken, channelId: channelId}
 	ret.slackClient = slack.New(ret.slackToken)
 	return ret
-	//= slack.New(slackToken)
 }
 
-//func SetTokenFromEnv(envstr string) bool {
-//	log.SetPrefix("[SetTokenFromEnv()]")
-//	slackToken = os.Getenv(envstr)
-//	// 没有token就退出。
-//	if slackToken == "" {
-//		return false
-//	}
-//	slackClient = slack.New(slackToken)
-//	return true
-//}
-
-//func SetChannel(cid string) {
-//	channelId = cid
-//}
-
-//func main() {
 // func SendMessage ,成功返回true, 失败返回false
 func (client *MySlackClient) SendMessage(message string) bool {
 	log.SetPrefix("[SendMessage()]")
+	defer log.SetPrefix("")
 	//for debug
 	//log.Println("slackToken:", slackToken)
 	// 判断SLACKTOKEN是否为空
@@ -68,8 +40,6 @@ func (client *MySlackClient) SendMessage(message string) bool {
 		log.Printf("SendMessage() failed: %s\n", err)
 		return false
 	}
-	//i,_ := strconv.ParseInt(timestamp,10,64)
-	//tm := time.Unix(i,0)
 	log.Printf("Message successfully sent to channel: %s. Message: %s\n", channelID, message)
 	return true
 
